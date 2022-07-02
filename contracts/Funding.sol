841092e49cf700b66adb90e974b30b630d6d2be4
dasom <50973005+dasom222g@users.noreply.github.com>
2022-07-03 04:35:51 +0900

    Fix mint logic

--- contracts/Funding.sol
+++ contracts/Funding.sol
@@ -3,9 +3,12 @@
 pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "MintToken.sol";
 
-contract Funding is Ownable {
+import "MintToken.sol";
+import "DataType.sol";
+import "Store.sol";
+
+contract Funding is Ownable, DataType {
     // 데드라인 시간에 if(모금액 >= 목표액) 모금액은 owner에게 전송
     // 데드라인 시간에 if(모금액 < 목표액) 모금액은 기부자들에게 환불
     // 프로퍼티: targetAmount, totalAmount, donationAddress/amountList, finishTime
@@ -15,22 +18,17 @@ contract Funding is Ownable {
     // 함수 refund : 목표액 달성 못했을 경우 기부자들에게 환불
 
     MintToken public mintTokenAddress;
-    TokenTypes[] public tokenDatas;
-    struct TokenTypes {
-        uint256 tokenId;
-        uint256 targetAmount;
-        uint256 totalAmount;
-        uint256 price;
-        uint256 finishTime;
-    }
+    Store public storeAddress;
     
-    constructor(MintToken _mintTokenAddress) {
+    constructor(MintToken _mintTokenAddress, Store _storeAddress) {
         mintTokenAddress = _mintTokenAddress;
-    }
-
-    function setTokenDatas() public {
-    //    string memory sfddf = mintTokenAddress.tokenURI(1);
+        storeAddress = _storeAddress;
     }
     
-    function recieve() public  {}
+    function recieve(uint256 _tokenId) public payable {
+        TokenTypes memory token = storeAddress.getTokenData(_tokenId);
+        require(block.timestamp < token.finishTime, "Already Finished.");
+        require(token.price == 0, "This token is not for sale.");
+        require(msg.value >= token.price, "Caller sent lower than price.");
+    }
 }
--- contracts/Store.sol
+++ contracts/Store.sol
@@ -7,15 +7,28 @@ import "DataType.sol";
 contract Store is DataType {
     // mapping (uint256 => uint256) public targetAmounts; // tokeId, targetAmount
     // mapping (uint256 => uint256) public totalAmounts; // tokeId, totalAmount
-    // mapping (address => uint256) public donations; // 기부자 address, tokeId
+    mapping (address => uint256) public donations; // 기부자 address, tokeId
     // mapping (uint256 => uint256) public tokenPrices; // tokeId, price
     // mapping (uint256 => uint256) public finishTime; // tokeId, finishTime
 
     TokenTypes[] public tokenDatas;
     uint256 public totalSupply;
 
+    constructor() {}
+
     function getTokenDatas() external view returns(TokenTypes[] memory) {
         return tokenDatas;
+    }
+
+    function getTokenData(uint256 _tokenId) public view returns (TokenTypes memory) {
+        TokenTypes memory tokenData;
+        for (uint256 i = 0; i < tokenDatas.length; i++) {
+            if (tokenDatas[i].tokenId == _tokenId) {
+                tokenData = tokenDatas[i];
+                break;
+            }
+        }
+        return tokenData;
     }
 
     function setTokenDatas(TokenTypes memory _token) external {
@@ -28,5 +41,12 @@ contract Store is DataType {
 
     function addTotalSupply() external {
         totalSupply++;
+    }
+    function getAmount(address _owner) external view returns(uint256) {
+        return donations[_owner];
+    }
+
+    function setDonation(address _owner, uint256 _amount) external {
+        donations[_owner] += _amount;
     }
 }
